fe0438e5f201f77968d017f25bf942baff6281a6
1982For <104335445+1982For@users.noreply.github.com>
2022-09-09 21:16:53 -0300

    Update main4.py

--- main4.py
+++ main4.py
@@ -2,6 +2,7 @@ import mysql.connector
 import pandas as pd 
 
 
+
 cnx = mysql.connector.connect(
     host = '3.89.36.150',
     user = 'e2122g3',
@@ -12,11 +13,11 @@ cnx = mysql.connector.connect(
 cur = cnx.cursor()
 
 cur.execute("""
-    DROP TABLE IF EXISTS PERGUNTAS_TCC; 
+    DROP TABLE IF EXISTS PERGUNTAS_TESTE; 
 """)
 
 cur.execute("""    
-    CREATE TABLE PERGUNTAS_TCC(
+    CREATE TABLE PERGUNTAS_TESTE(
             PERGUNTAS_CARIMBO TEXT NOT NULL,
             PERGUNTAS_ID_RESPOSTA INTEGER,
             PERGUNTAS_NOME TEXT NOT NULL,
@@ -24,38 +25,24 @@ cur.execute("""
     );
 """)
 # df.groupby(['col1', 'col2']).size()
-#  Tabela de PERGUNTAS_TCC
-url_or_file='https://docs.google.com/spreadsheets/d/e/2PACX-1vQrGR2RwQBQAbt3Mzu0UbgKMGSY1hxXtt8rV1fcLzrwjRy2KBDUaSOoT5EKt-j0t6cxZ0KIz-4O0ZVf/pub?gid=891049362&single=true&output=csv'
+#  Tabela de PERGUNTAS_TESTE
+url_or_file = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQrGR2RwQBQAbt3Mzu0UbgKMGSY1hxXtt8rV1fcLzrwjRy2KBDUaSOoT5EKt-j0t6cxZ0KIz-4O0ZVf/pub?gid=1689922439&single=true&output=csv'
+colunas = list(['CARIMBO','IDRESPOSTA','PERGUNTA','RESPOSTA'])
 
-dfok=pd.DataFrame()
-
-dfok['Carimbo de data/hora']=''
-dfok['Answer']=''
-dfok['Question']=''
-dfok['Questionid']=''
-
-for i in range(1,14):
-    colunas=[0,i]
-    df = pd.read_csv(url_or_file, header=0,usecols=colunas)
-    pergunta=df.columns[1]
-    df['Question']=pergunta
-    df['Questionid']=i
-    df = df.rename(columns={ pergunta : 'Answer'})    
-    dfok = (pd.concat([dfok, df],ignore_index = True))
-
-    #print(dfok.dtypes)
+df = pd.read_csv(url_or_file)
 
 values = []
-for index,row in dfok.iterrows():
-
-    CARIMBO = dfok[[ 'Carimbo de data/hora']]  
-    IDRESPOSTA = dfok[[ 'Questionid']]  
-    PERGUNTA = dfok[[ 'Answer']]  
-    RESPOSTA = dfok[[ 'Question']]   
+for index,row in df.iterrows():
+    CARIMBO = row.CARIMBO
+    IDRESPOSTA = row.IDRESPOSTA
+    PERGUNTA = row.PERGUNTA
+    RESPOSTA = row.RESPOSTA
     values.append((CARIMBO,IDRESPOSTA,PERGUNTA,RESPOSTA))
-    
         
 insert_values = "".join(str(values).strip('[]'))
-sql=(f"INSERT INTO PERGUNTAS_TCC (PERGUNTAS_CARIMBO,PERGUNTAS_ID_RESPOSTA,PERGUNTAS_NOME,PERGUNTAS_RESPOSTA) VALUES {insert_values}")
+sql=(f"INSERT INTO PERGUNTAS_TESTE (PERGUNTAS_CARIMBO,PERGUNTAS_ID_RESPOSTA,PERGUNTAS_NOME,PERGUNTAS_RESPOSTA) VALUES {insert_values}")
+
+print(insert_values)
+# .
 cur.execute(sql)
 cnx.commit()
