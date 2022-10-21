44db4c75b1e17bcd62f973eaa23eb51677be8b80
Fernando Oliveira da Rosa <104335445+1982For@users.noreply.github.com>
2022-10-20 22:49:14 -0300

    Update colunas-replace-codigos.py

--- projetoentra21/testes/colunas-replace-codigos.py
+++ projetoentra21/testes/colunas-replace-codigos.py
@@ -76,17 +76,7 @@ class TrocaWordId(QuestionID,Word):
     dict = Dict(
         'question_id' : 8
         'empresas' : {
-            'Empresa 1': 'Senior Sistemas',
-            'Empresa 2': 'Philips',
-            'Empresa 3': 'Ailos',
-            'Empresa 4': 'Capgemini',
-            'Empresa 5': 'Ambev Tech',
-            'Empresa 6': 'Warren',
-            'Empresa 7': 'DataInfo',
-            'Empresa 8': 'Farmácias App',
-            'Empresa 9': 'Havan Labs',
-            'Empresa 10': 'T-Systems',
-            'Empresa 11': 'Unifique',
+            
         }
         )
     
