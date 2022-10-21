ad8737cad43ccb4d6541552127717d9c024b3095
Fernando Oliveira da Rosa <104335445+1982For@users.noreply.github.com>
2022-10-21 15:55:43 -0300

    Update main4.py

--- projetoentra21/testes/main4.py
+++ projetoentra21/testes/main4.py
@@ -4,10 +4,10 @@ import pandas as pd
 
 
 cnx = mysql.connector.connect(
-    host = '3.89.36.150',
-    user = 'e2122g3',
-    password = 'e2122g3@16@ago',
-    database = 'e2122g3'
+    host = '',
+    user = '',
+    password = '',
+    database = ''
     )
 
 cur = cnx.cursor()
@@ -26,7 +26,7 @@ cur.execute("""
 """)
 # df.groupby(['col1', 'col2']).size()
 #  Tabela de PERGUNTAS_TESTE
-url_or_file = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQrGR2RwQBQAbt3Mzu0UbgKMGSY1hxXtt8rV1fcLzrwjRy2KBDUaSOoT5EKt-j0t6cxZ0KIz-4O0ZVf/pub?gid=1689922439&single=true&output=csv'
+url_or_file = ''
 colunas = list(['CARIMBO','IDRESPOSTA','PERGUNTA','RESPOSTA'])
 
 df = pd.read_csv(url_or_file)
