3e4c73b6bfbcc392e7e3b0569ac6f3715dc00c7f
Fernando Oliveira da Rosa <104335445+1982For@users.noreply.github.com>
2022-10-24 17:59:14 -0300

    Update projeto-tcc-g3-dados.py

--- projetoentra21/projeto-tcc-g3-dados.py
+++ projetoentra21/projeto-tcc-g3-dados.py
@@ -2,10 +2,6 @@
 import mysql.connector
 import pandas as pd
 
-
-def pp(a):
-    print('a', end='')
-
 # - Importando planilha google docs
 url_or_file = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQrGR2RwQBQAbt3Mzu0UbgKMGSY1hxXtt8rV1fcLzrwjRy2KBDUaSOoT5EKt-j0t6cxZ0KIz-4O0ZVf/pub?gid=891049362&single=true&output=csv'
 
@@ -33,25 +29,16 @@ for i in range(1, 14):
     df = (pd.concat([df, df_coluna_by_coluna], ignore_index=True))
     df['Carimbo_de_data_hora'] = (pd.to_datetime(df['Carimbo_de_data_hora']))
     df['Questionid'] = (pd.to_numeric(df['Questionid']))
-# Ponto de verificação
-print(len(df))
-
-pp('b1')
 
 # Abaixo criamos uma codificação para os nomes 
 # de empresas e outros detalhes.
 
 def verifica(base8, empresa):
-    pp('b2')
     ret = 'None'
     for a, b in base8.items():
         if (str.strip(a) == str.strip(empresa)):
             ret = b
-    print(f'verifica() --> Base:{empresa}:,:{ret}:')
     return ret
-
-
-pp('b3')
 
 # Aqui criamos um dicionário de dados
 # para suprimir/alterar o nome da empresa
@@ -70,10 +57,6 @@ base8 = {
     'Empresa11': 'Empresa 11'
 }
 
-
-pp('b4')
-
-
 # dataframe Nova Serialização
 dfns = pd.DataFrame()
 
@@ -90,7 +73,6 @@ for index, row in df.iterrows():
         qid.append(row.Questionid)
         ans.append(answerseparada)
         if (row.Questionid == 8):
-            print(f'AnswerSeparada:{answerseparada}:')
             ans_code.append(verifica(base8, answerseparada))
         
         else:
@@ -105,14 +87,6 @@ dfns['AnswerCode'] = ans_code
 # A linha abaixo é responsável por excluir os espaços em branco após a vírgula nas respostas coletadas.
 dfns['AnswerCode'] = dfns['AnswerCode'].str.strip()
 
-print(dfns.shape)
-
-
-print("aqui")
-print(dfns)
-print("ok")
-
-pp('mysql')
 # Conexão com o servidor
 cnx = mysql.connector.connect(
     host='',
@@ -136,14 +110,11 @@ cur.execute("""
             PERGUNTAS_RESPOSTA TEXT NOT NULL
     );
 """)
-pp('mysql create database')
-
 
 divisoes = int((len(dfns))/1000+1)
 
 contador = 0
 print(divisoes)
-
 
 def insere_valores(inicio, fim):
     contador = 0
@@ -153,7 +124,6 @@ def insere_valores(inicio, fim):
     values = []
     for index, row in dfns[inicio:fim].iterrows():
         for xresp in (list(str.split(row.AnswerCode, sep=','))):
-            pp('F')
             CARIMBO = row.Carimbo_de_data_hora
             IDRESPOSTA = row.Questionid
             PERGUNTA = row.Question
@@ -163,18 +133,13 @@ def insere_valores(inicio, fim):
             
     insert_values = "".join(str(values).strip('[]'))
     # Inserindo os valores
-    sql = (
-        f"INSERT INTO PERGUNTAS_TCC (PERGUNTAS_CARIMBO,PERGUNTAS_ID_RESPOSTA,PERGUNTAS_NOME,PERGUNTAS_RESPOSTA) VALUES {insert_values}")
-
-    print(f'Contador: {contador}')
+    sql = (f"INSERT INTO PERGUNTAS_TCC (PERGUNTAS_CARIMBO,PERGUNTAS_ID_RESPOSTA,PERGUNTAS_NOME,PERGUNTAS_RESPOSTA) VALUES {insert_values}")
 
     cur.execute(sql)
     cnx.commit()
 
 
 divisoes = int((len(dfns))/1000+1)
-
-print(divisoes)
 
 total = int(len(dfns))
 passo = int(len(dfns)/divisoes)
@@ -184,10 +149,8 @@ x, inicio, fim = 0, 0, 0
 lista = []
 
 while saldo > 0:
-    pp('G')
     fim = fim+passo
-    print(f'saldo: {saldo} passo: {passo}')
-
+    
     if saldo < passo:
         fim = inicio + (saldo-1)
     else:
@@ -203,23 +166,8 @@ while saldo > 0:
     inicio = fim + 1
 
 for x, y in lista:
-    print(f'*************: {x},{y} = {y-x}')
     insere_valores(x, y)
 
-# Conexão com o servidor
-cnx = mysql.connector.connect(
-    host='',
-    user='',
-    password='',
-    database=''
-)
-cur = cnx.cursor()
-pp('H')
-cur.execute("""
-    SELECT COUNT(*) FROM PERGUNTAS_TCC; 
-""")
 
-pp('I')
 
-print(f'Contador Mysql: {cur.fetchone()} ')
 
